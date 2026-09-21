a34a36e700fe353f4a721070895fa9607509fe80
paparapas <andre.rendas@icloud.com>
2026-09-21 13:17:00 +0100

    Update 0_Liga Scoreboard.py

--- pages/0_Liga Scoreboard.py
+++ pages/0_Liga Scoreboard.py
@@ -21,7 +21,7 @@ if os.path.exists(logo_path):
 @st.cache_data
 def load_data():
     try:
-        with open('bbpt_master_db.json', 'r', encoding='utf-8') as f: return json.load(f)
+        with open('bbpt_master_db_season2.json', 'r', encoding='utf-8') as f: return json.load(f)
     except FileNotFoundError: return None
 
 db = load_data()
